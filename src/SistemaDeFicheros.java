faf87de5151fbab9b2c49346e30fb921db30748f
janoagd <alu.74999@usj.es>
2016-12-07 12:46:52 +0100

    creaArchivos terminado

--- src/SistemaDeFicheros.java
+++ src/SistemaDeFicheros.java
@@ -7,16 +7,19 @@ public class SistemaDeFicheros {
 	Metadato[] metadatos;
 	private int tamanyo = 20;
 	private int tamCluster=4;
+	static Directorio raiz;
 	
 	public SistemaDeFicheros()
 	{			
 		datos = new Cluster[tamanyo];
 		metadatos= new Metadato[tamanyo];
-		
-		for(int i=0;i<tamanyo;i++)
-		{
+		raiz = new Directorio("C:");
+		for(int i=0;i<tamanyo;i++){
 			metadatos[i] = new Metadato(i);
 		}
+		datos[0]=raiz;
+		metadatos[0].disponible=false;
+		metadatos[0].fin=true;
 	}
 	
 	public void mostrarMetadatos()
@@ -38,24 +41,46 @@ public class SistemaDeFicheros {
 		cadena=contenido.toCharArray();
 		int tamCadena=cadena.length;
 		int numClusters=tamCadena/tamCluster;
+		if(tamCadena%tamCluster!=0) numClusters++;
 		int i=0;
 		int a=0;
 		int b=0;
-		int posAnterior=0;
-		while(numClusters!=0){
-			numClusters--;
-			char[] parte=new char[tamCluster];
-			for(a=0; a<tamCluster; a++){
-				parte[a]=cadena[b];
-				b++;
+		int clusterAnterior=-1;
+		for(i=0; i<metadatos.length; i++){
+			if(metadatos[i].disponible==true) a++;
+		}
+		if(a<numClusters) System.out.println("no hay espacio suficiente");
+		else{
+			i=0;
+			int posicionRaiz=0;
+			while(raiz.carpeta[posicionRaiz].nombre!=null){
+				posicionRaiz++;
 			}
-			String parteContenido=String.valueOf(parte);
-			while(metadatos[i].disponible!=true)
-			{
-				i++;
+			while(numClusters!=0){
+				numClusters--;
+				char[] parte=new char[tamCluster];
+				for(a=0; a<tamCluster; a++){
+					parte[a]=cadena[b];
+					b++;
+				}
+				String parteContenido=String.valueOf(parte);
+				while(metadatos[i].disponible!=true)
+				{
+					i++;
+				}
+				metadatos[i].disponible = false;
+				datos[i]= new Archivo(parteContenido);
+				if(clusterAnterior!=-1){
+					if(numClusters==0) metadatos[i].fin=true;
+					else metadatos[clusterAnterior].siguiente=metadatos[i];
+				}else{
+					raiz.carpeta[posicionRaiz].clusterInicio=i;
+					raiz.carpeta[posicionRaiz].nombre=contenido;
+					raiz.carpeta[posicionRaiz].tipo='A';
+					datos[0]=raiz;
+				}
+				clusterAnterior=i;
 			}
-			metadatos[i].disponible = false;
-			datos[i]= new Archivo(parteContenido);
 		}
 	}
 	
